816427aba8ff2ebb385fa2c8546317aa956250db
ddsec00 <ddavlatkhojaev@gmail.com>
2026-06-04 11:43:32 +0600

    the first simple script

---
+++ main.py
@@ -0,0 +1,16 @@
+import socket
+
+target = "127.0.0.1"
+port = 80
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.settimeout(1)
+
+result = sock.connect_ex((target, port))
+
+if result == 0:
+    print(f"{port} is OPEN")
+else:
+    print(f"{port} is CLOSED")
+
+sock.close()
