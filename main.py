9ace3686f4f92804bb7700314f4b298180f89720
ddsec00 <ddavlatkhojaev@gmail.com>
2026-06-04 11:44:36 +0600

    the second simple script for all ports

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import socket
 
 target = "127.0.0.1"
-port = 80
+ports = [22, 80, 443, 21, 25]
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.settimeout(1)
+print(f"Scanning {target}...\n")
 
-result = sock.connect_ex((target, port))
+for port in ports:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
 
-if result == 0:
-    print(f"{port} is OPEN")
-else:
-    print(f"{port} is CLOSED")
+    result = sock.connect_ex((target, port))
 
-sock.close()
+    if result == 0:
+        print(f"{port}/tcp OPEN")
+    else:
+        print(f"{port}/tcp CLOSED")
+
+    sock.close()
